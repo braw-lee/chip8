f46683ce24baf374b1aa57bface52a3e61d5c787
brawlee <kushalpal109@gmail.com>
2023-04-17 10:23:15 +0530

    Removed a bug in config.h , now all classes have access to same instance of config

--- include/config.h
+++ include/config.h
@@ -1,6 +1,8 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
+#include <cstdint>
+
 enum Version
 {
 	ORIGINAL,
@@ -13,16 +15,29 @@ enum GameState
 	RUNNING
 };
 
+struct ColorCombo
+{
+	//foreground color , default orange
+	uint8_t fg_red{0};
+	uint8_t fg_green{0};
+	uint8_t fg_blue{255};
+
+	//background color , default black
+	uint8_t bg_red{255};
+	uint8_t bg_green{255};
+	uint8_t bg_blue{255};
+};
+
 struct Config
 {
 	Version currVersion{CHIP_48};
 	int volume{3000};
-	int instructionsPerSecond{700};
+	int instructionsPerSecond{900};
 	int framesPerSecond{60};
-	int gameState{0};
+	int gameState{RUNNING};
+	ColorCombo currentCombo{};
 };
 
-
-static Config configurations;
+extern Config configurations;
 
 #endif
